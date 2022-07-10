b9285c1f46f8d90d112233cade7b2998c935911f
MorrisLin <lin.morris@external.stellantis.com>
2022-07-11 00:40:17 +0800

    gitflow-feature-stash: map_scene

---
+++ app/src/main/java/com/cartoonhero/privatekitchen_android/stage/scene/map/PhMapScene.kt
@@ -0,0 +1,12 @@
+package com.cartoonhero.privatekitchen_android.stage.scene.map
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.cartoonhero.privatekitchen_android.R
+
+class PhMapScene : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.phone_map_scene)
+    }
+}
